f0c5c71056231bcc910735d746547e38e5e23b7b
harryTheWizzard <43062552+harryTheWizzard@users.noreply.github.com>
2022-03-06 18:18:36 +0100

    Implement Testing
    test for cavalryUnit
    to see how the testing works

---
+++ src/test/java/wargamesdel1/units/CavalryUnitTest.java
@@ -0,0 +1,17 @@
+package wargamesdel1.units;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CavalryUnitTest {
+
+
+    @Test
+    void getAttackBonus(){
+    }
+
+    @Test
+    void getResistBonus() {
+    }
+}
