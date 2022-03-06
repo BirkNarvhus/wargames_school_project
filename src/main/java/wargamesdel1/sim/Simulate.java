dbf581c97eea099d6888afcaaf73b8791e524d70
harryTheWizzard <43062552+harryTheWizzard@users.noreply.github.com>
2022-03-06 18:29:10 +0100

    added a simulate class and a run config

---
+++ src/main/java/wargamesdel1/sim/Simulate.java
@@ -0,0 +1,10 @@
+package wargamesdel1.sim;
+
+import wargamesdel1.units.ComanderUnit;
+
+public class Simulate {
+    public static void main(String[] args) {
+        ComanderUnit c = new ComanderUnit("test 1", 150);
+        System.out.println(c);
+    }
+}
